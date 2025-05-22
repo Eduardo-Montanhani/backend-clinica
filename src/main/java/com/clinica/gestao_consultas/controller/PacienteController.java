3befb1a0dbf39e416bcd99f436723b21216005f1
Eduardo-Montanhani <montanhani81@gmail.com>
2025-05-22 11:32:06 -0300

    ajustando rotas para mostrar para paciente que tem consultas em abertos nao podem ser excluidos

--- src/main/java/com/clinica/gestao_consultas/controller/PacienteController.java
+++ src/main/java/com/clinica/gestao_consultas/controller/PacienteController.java
@@ -1,12 +1,14 @@
 package com.clinica.gestao_consultas.controller;
 
 import com.clinica.gestao_consultas.model.Paciente;
+import com.clinica.gestao_consultas.repository.ConsultaRepository;
 import com.clinica.gestao_consultas.repository.PacienteRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,6 +18,11 @@ public class PacienteController {
 
     @Autowired
     private PacienteRepository pacienteRepository;
+    private final ConsultaRepository consultaRepository;
+
+    public PacienteController(ConsultaRepository consultaRepository) {
+        this.consultaRepository = consultaRepository;
+    }
 
     //Get api/pacientes
     @GetMapping
@@ -53,11 +60,20 @@ public class PacienteController {
 
     //DELETE /api/pacientes/{id}
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deletar(@PathVariable Long id) {
-        if (pacienteRepository.existsById(id)) {
-            pacienteRepository.deleteById(id);
-            return ResponseEntity.noContent().build();
+    public ResponseEntity<?> deletar(@PathVariable Long id) {
+        if (!pacienteRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.notFound().build();
+
+        // Verifica se paciente tem consultas pendentes (futuras)
+        var consultasPendentes = consultaRepository.findByPacienteIdAndDataHoraAfter(id, LocalDateTime.now());
+
+        if (!consultasPendentes.isEmpty()) {
+            return ResponseEntity.badRequest()
+                    .body("Não é possível excluir paciente: existe(m) consulta(s) pendente(s).");
+        }
+
+        pacienteRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/clinica/gestao_consultas/repository/ConsultaRepository.java
+++ src/main/java/com/clinica/gestao_consultas/repository/ConsultaRepository.java
@@ -5,6 +5,12 @@ import com.clinica.gestao_consultas.model.Consulta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.awt.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+    // Consultas futuras do paciente
+    List<Consulta> findByPacienteIdAndDataHoraAfter(Long pacienteId, LocalDateTime now);
 }
